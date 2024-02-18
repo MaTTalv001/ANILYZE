9397c08bcbbc8e11c11c3cc45879f90bb3763999
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-18 17:12:44 +0900

    ページネーション

--- backend/app/controllers/api/v1/works_controller.rb
+++ backend/app/controllers/api/v1/works_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::WorksController < ApplicationController
   def index
-    @works = Work.includes(:people).all
-    render json: @works.as_json(include: :people)
+    @works = Work.includes(:people).page(params[:page] || 1).per(params[:per] || 25)
+    render json: {
+  works: @works.as_json(include: :people),
+  total_pages: @works.total_pages,
+  current_page: @works.current_page,
+  total_count: @works.total_count
+}
   end
 end
